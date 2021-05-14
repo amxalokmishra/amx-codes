dc69f3ddbf6ee4f7d6f923ff752c1ac789ed816c
amxalokmishra <58151872+amxalokmishra@users.noreply.github.com>
2021-05-14 16:42:52 +0530

    div3.cpp

---
+++ div3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=100;i++)
+    {
+        if(i%3==0)
+        continue;
+        cout<<i<<endl;
+    }
+    return 0;
+}
